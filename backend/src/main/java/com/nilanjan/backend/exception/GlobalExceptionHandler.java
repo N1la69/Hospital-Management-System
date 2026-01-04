8b61af9bb553f8b7912f97ee253cc8601abf13c9
Nilanjan Saha <nilanjan.s.1920@gmail.com>
2026-01-04 11:47:24 +0530

    SecurityConfig and GlobalException skeleton added

---
+++ backend/src/main/java/com/nilanjan/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.nilanjan.backend.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleException(Exception ex){
+        return ResponseEntity.internalServerError().body(ex.getMessage());
+    }
+}
---
+++ backend/src/main/java/com/nilanjan/backend/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.nilanjan.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+public class SecurityConfig {
+    
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth/**","/swagger-ui/**","/v3/api-docs/**").permitAll()
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
+}
